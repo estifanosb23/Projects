2c7180b761624cd44a20389ebe57ad85d713f8e1
Estifanos Befekadu <b.estifanos23@gmail.com>
2026-03-18 18:23:42 -0400

    Update and rename README.java to README.md
    Removed Java class and replaced with Markdown content.

--- README.java
+++
@@ -1,9 +0,0 @@
-public class README {
-    public static void main(String[] args) {
-        System.out.println("Projects");
-        System.out.println();
-        System.out.println("Here are some of the coding projects I've worked on.");
-        System.out.println();
-        System.out.println("Enjoy.");
-    }
-}
